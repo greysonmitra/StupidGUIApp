8651ff285a038b2f8290b9571ae42fe1cb14b950
greysonmitra <greyson.mitra@outlook.com>
2015-11-09 10:03:33 -0700

    Added a proper window
    Proper window uses buttons and type fields for real now

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -6,22 +6,31 @@ import gui.controller.GUIController;
 /**
  * 
  * @author gmit3891
- *@version 0.1 Nov 9, 2015
+ *@version 0.2 Nov 9, 2015 updated the setupFrame to load the contentPane with GUIPanel
  */
 public class GUIFrame extends JFrame
 {
 	private GUIController baseController;
+	private GUIPanel basePanel;
 	
 	public GUIFrame(GUIController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GUIPanel(baseController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setSize(400,400);
-		this.setResizable(false);
-		this.setVisible(true);
+		this.setContentPane(basePanel); //Must be the first line of setupFrame (Actually installs the panel in frame)! So it initializes first.
+		this.setSize(400,400); 			//Just find a reasonable size for the type of App.
+		this.setTitle("YOOOO");
+		this.setResizable(false);       //Not required
+		this.setVisible(true);		    //Must be last line of setupFrame! So that is sets up the frame then displays it duh.
+	} 
+	
+	public GUIController getbaseController()
+	{
+		return baseController;
 	}
 }
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,6 +1,9 @@
 package gui.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout; //For layout
 import gui.controller.GUIController;
 
 /**
@@ -12,9 +15,44 @@ import gui.controller.GUIController;
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton firstButton;
+	private JTextField firstField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
+		
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Please do not click the button");
+		firstField = new JTextField("words can be typed here");
+	
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	/**
+	 *Helper method to load all GUI components into Panel 
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstField);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -102, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstField, -252, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 46, SpringLayout.SOUTH, firstField);
+		baseLayout.putConstraint(SpringLayout.WEST, firstField, 45, SpringLayout.WEST, this);
+		
+	}
+	
+	private void setupListeners()
+	{
+		
 	}
 }
